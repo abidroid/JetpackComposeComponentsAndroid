ad9c2ad465c2a845a5eb2b108061269e77d53109
Muhammad Abid <abidroid1@gmail.com>
2024-05-30 21:49:42 +0500

    Row, Column and Box

--- app/src/main/java/com/example/jetpackcomposecomponentsandroid/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecomponentsandroid/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposecomponentsandroid.ui.theme.JetpackComposeComponentsAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +40,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+    Column {
+
+        Row {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+            Text(
+                text = "yes"
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .background(color = Color.Red)
+                .width(300.dp)
+                .height(300.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(color = Color.Green)
+                    .width(200.dp)
+                    .height(200.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .background(color = Color.Blue)
+                        .width(100.dp)
+                        .height(100.dp)
+                ) {
+
+                }
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
