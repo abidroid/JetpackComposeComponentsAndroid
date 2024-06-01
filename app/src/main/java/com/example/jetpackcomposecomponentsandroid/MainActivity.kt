31a5c1614b88a2d9c45dc027b58a3dbaacc69858
Muhammad Abid <abidroid1@gmail.com>
2024-06-01 14:40:20 +0500

    DMC

--- app/src/main/java/com/example/jetpackcomposecomponentsandroid/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecomponentsandroid/MainActivity.kt
@@ -16,6 +16,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
@@ -30,11 +33,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposecomponentsandroid.ui.theme.JetpackComposeComponentsAndroidTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,8 +54,8 @@ class MainActivity : ComponentActivity() {
 
                     //MyAlignments(name = "Android", modifier = Modifier.padding(innerPadding))
                     //MyButtons(name = "My Buttons", modifier = Modifier.padding(innerPadding))
-                    MyTextFields(name = "My TextFields", modifier = Modifier.padding(innerPadding))
-
+                    //MyTextFields(name = "My TextFields", modifier = Modifier.padding(innerPadding))
+                    Dmc(name = "Dmc", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -227,13 +230,145 @@ fun MyTextFields(name: String, modifier: Modifier = Modifier) {
     ) {
         Text(text = "TextFields")
         Spacer(modifier = Modifier.size(10.dp))
-        TextField(value = tf.value, onValueChange = {
-            tf.value = it
-        },
+        TextField(
+            value = tf.value, onValueChange = {
+                tf.value = it
+            },
             label = { Text(text = "Enter your name", color = Color.White) },
             modifier = Modifier.fillMaxWidth()
 
         )
+    }
+}
+
+@Composable
+fun Dmc(name: String, modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState() // Create a ScrollState
+
+    var eng = remember {
+        mutableStateOf("")
+    }
+    var urdu = remember {
+        mutableStateOf("")
+    }
+    var math = remember {
+        mutableStateOf("")
+    }
+    var science = remember {
+        mutableStateOf("")
+    }
+    var gk = remember {
+        mutableStateOf("")
+    }
+
+    var obtainedMarks = remember {
+        mutableStateOf("")
+    }
+
+    var percentage = remember {
+        mutableStateOf("")
+    }
+
+    var grade = remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+            .background(color = Color.White)
+            .verticalScroll(scrollState),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Text(text = "DMC")
+        Spacer(modifier = Modifier.size(10.dp))
+        TextField(
+            value = eng.value, onValueChange = { eng.value = it },
+            label = { Text(text = "English") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        TextField(
+            value = urdu.value, onValueChange = { urdu.value = it },
+            label = { Text(text = "Urdu") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        TextField(
+            value = math.value, onValueChange = { math.value = it },
+            label = { Text(text = "Math") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        TextField(
+            value = science.value, onValueChange = { science.value = it },
+            label = { Text(text = "Science") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        TextField(
+            value = gk.value, onValueChange = { gk.value = it },
+            label = { Text(text = "GK") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+
+        Row {
+            Button(onClick = {
+                eng.value = ""
+                urdu.value = ""
+                math.value = ""
+                science.value = ""
+                gk.value = ""
+            }, modifier = Modifier.weight(1f)) {
+                Text(text = "Clear")
+            }
+            Spacer(modifier = Modifier.size(10.dp))
+            Button(onClick = {
+
+                val obtMarks: Int = eng.value.toInt() + urdu.value.toInt() + math.value.toInt() + science.value.toInt() + gk.value.toInt()
+                obtainedMarks.value = obtMarks.toString()
+
+                val per: Double = (obtMarks / 500.0) * 100
+                percentage.value = per.toString()
+
+                if( per >= 80){
+                    grade.value = "A"
+                }
+
+                if( per >= 70 && per < 80){
+                    grade.value = "B"
+                }
+
+                if( per >= 60 && per < 70){
+                    grade.value = "C"
+
+                }
+
+                if( per >= 40 && per < 60) {
+                    grade.value = "D"
+
+                }
+
+                if(per < 40){
+                    grade.value = "Fail"
+                }
+            }, modifier = Modifier.weight(1f)) {
+                Text(text = "Calculate")
+            }
+
+        }
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(text = obtainedMarks.value)
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(text = percentage.value)
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(text = grade.value)
+
     }
 }
 
@@ -245,6 +380,7 @@ fun GreetingPreview() {
         //MyLayouts(name = "Android")
         //MyAlignments(name = "Android", modifier = Modifier)
         //MyButtons(name = "My Buttons", modifier = Modifier)
-        MyTextFields(name = "My TextFields", modifier = Modifier)
+        //MyTextFields(name = "My TextFields", modifier = Modifier)
+        Dmc(name = "Dmc")
     }
 }
